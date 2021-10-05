d2fa4b6b38b9b41d180eb963de96025c013487b4
Supporterino <lars@roth-kl.de>
2021-10-05 18:13:38 +0000

    finalise outputlayer

--- src/cnn/layers/outputLayer.ts
+++ src/cnn/layers/outputLayer.ts
@@ -7,6 +7,12 @@ export class OutputLayer extends InterpretingLayer {
   }
 
   interpret(data: Image): Map<number, number> {
-    return new Map<number, number>();
+    const output = new Map<number, number>();
+
+    for (let index = 0; index < this._numNodes; index++) {
+      output.set(index, this._nodes[index].calculateValue(data.getValues()));
+    }
+
+    return output;
   }
 }
