d395f26d28ea71e7299e1754113cce5c6ddcd47a
Supporterino <lars@roth-kl.de>
2021-10-05 18:31:17 +0000

    Add constructor to `cnn` class

--- src/cnn/cnn.ts
+++ src/cnn/cnn.ts
@@ -1,3 +1,16 @@
+import { Layer, OperationMode } from '.';
+import { ImageHandler } from '../img';
+
 export class CNN {
   private _name: string;
+  private _mode: OperationMode;
+  private _layers: Array<Layer>;
+  private _imageHandler: ImageHandler;
+
+  constructor(name: string, mode: OperationMode, layers: Array<Layer>, handler: ImageHandler) {
+    this._name = name;
+    this._mode = mode;
+    this._layers = layers;
+    this._imageHandler = handler;
+  }
 }
