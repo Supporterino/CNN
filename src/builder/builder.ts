8a152a577a5d11185fdb2ff9b106f4a2fb876039
Supporterino <lars@roth-kl.de>
2021-10-05 18:29:22 +0000

    Add operation mode to builder

--- src/builder/builder.ts
+++ src/builder/builder.ts
@@ -1,19 +1,21 @@
 import { prompt, QuestionCollection } from 'inquirer';
-import { ActivasionTypes, ConvolutionLayer, Layer, NormalizingLayer, OutputLayer } from '../cnn';
+import { ActivasionTypes, ConvolutionLayer, Layer, NormalizingLayer, OperationMode, OutputLayer } from '../cnn';
 import { ImageHandler } from '../img';
 
 export class Builder {
   private _pixels: number;
-  private _learning_rate: number;
-  private _layer_count: number;
-  private _activasion_function: ActivasionTypes;
+  private _learningRate: number;
+  private _layerCount: number;
+  private _activasionFunction: ActivasionTypes;
   private _imageHandler: ImageHandler;
+  private _operationMode: OperationMode;
 
   constructor(options: any) {
     this._pixels = options.pixels;
-    this._learning_rate = options.learning_rate;
-    this._layer_count = options.layer_count;
-    this._activasion_function = options.activasion_function;
+    this._learningRate = options.learning_rate;
+    this._layerCount = options.layer_count;
+    this._activasionFunction = options.activasion_function;
+    this._operationMode = options.operation_mode;
     this._imageHandler = new ImageHandler(this._pixels);
   }
 
@@ -24,22 +26,22 @@ export class Builder {
   constructLayers(): Array<Layer> {
     const output = new Array<Layer>();
 
-    output.push(new NormalizingLayer(this._imageHandler.sizeSquared, this._activasion_function, 0));
+    output.push(new NormalizingLayer(this._imageHandler.sizeSquared, this._activasionFunction, 0));
 
     let prevLayer = null;
-    for (let index = 0; index < this._layer_count; index++) {
+    for (let index = 0; index < this._layerCount; index++) {
       if (index == 0) {
-        const newLayer: ConvolutionLayer = new ConvolutionLayer(this._imageHandler.sizeSquared, this._activasion_function, index + 1);
+        const newLayer: ConvolutionLayer = new ConvolutionLayer(this._imageHandler.sizeSquared, this._activasionFunction, index + 1);
         output.push(newLayer);
         prevLayer = newLayer;
       } else {
-        const newLayer: ConvolutionLayer = new ConvolutionLayer(prevLayer?.numNodes!, this._activasion_function, index + 1);
+        const newLayer: ConvolutionLayer = new ConvolutionLayer(prevLayer?.numNodes!, this._activasionFunction, index + 1);
         output.push(newLayer);
         prevLayer = newLayer;
       }
     }
 
-    output.push(new OutputLayer(prevLayer?.numNodes!, this._activasion_function, this._layer_count + 2));
+    output.push(new OutputLayer(prevLayer?.numNodes!, this._activasionFunction, this._layerCount + 2));
 
     return output;
   }
--- src/cnn/types.ts
+++ src/cnn/types.ts
@@ -2,3 +2,9 @@ export enum ActivasionTypes {
   SIGMOID,
   RELU
 }
+
+export enum OperationMode {
+  PRODUCTIVE,
+  TRAINING,
+  TEST
+}
